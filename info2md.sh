be22decba87cde1f96c89c5582e126482df89abe
Igor Chebykin <ichebykin@accesssoftek.com>
2019-12-08 09:18:03 -0800

    First edit

---
+++ info2md.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+__DIR__=`dirname $0`
+__DIR__=`(cd $__DIR__; pwd)`
+
+if [ -z "$1" ]; then
+    exit 1
+fi
+
+lcov --no-list-full-path --list $1 \
+    | ${__DIR__}/info2md.pl
